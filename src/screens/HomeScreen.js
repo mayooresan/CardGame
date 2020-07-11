aff554405c9a024db9c4658686d8745272da5bc3
Jeyakumaran Mayooresan <mayu3g@gmail.com>
2020-07-11 13:54:00 +0800

    grid constructed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,21 +5,87 @@ import {
   Text,
   View,
   Button,
+  FlatList,
+  Dimensions
 } from 'react-native';
 import CAColors from '../res/CAColors';
 import CAStrings from '../res/CAStrings';
 import CAStyles from '../res/CAStyles';
 
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: '?',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: '?',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: '?',
+    },
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: '?',
+      },
+      {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: '?',
+      },
+      {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: '?',
+      },
+      {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: '?',
+      },
+      {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: '?',
+      },
+      {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: '?',
+      },
+      {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: '?',
+      },
+      {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: '?',
+      },
+      {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: '?',
+      },
+
+  ];
+  
+  const Item = (props ) => (
+    <View style={[{ backgroundColor: '#f9c2ff', height:100, margin:8}, CAStyles.ALIGN_CENTER_ROW]} width={props.width}>
+      <Text style={styles.title}>{props.title}</Text>
+    </View>
+  );
+
+
 const HomeScreen = () => {
    const [steps, setSteps] = useState(0)
+   const screenWidth = Math.round(Dimensions.get('window').width) - 48;
 
     useEffect(() => {
-       
+       console.log(screenWidth)
     }, [])
 
     resetSteps = () => {
         setSteps(0)
     }
+
+    const renderItem = ({ item }) => (
+        <Item title={item.title} width={screenWidth/3}/>
+    );
 
     return(
         <SafeAreaView style={styles.parentContainer}>
@@ -35,9 +101,12 @@ const HomeScreen = () => {
                 </View> 
             </View>
 
-            <View style={{backgroundColor:CAColors.BLUE}}>
-                <Text style={{color:CAColors.WHITE}}>?</Text>
-            </View>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                numColumns={3}
+            />
             <Text>{CAStrings.TEST_STRING}</Text>
         </SafeAreaView>
     )
@@ -47,7 +116,14 @@ const styles = StyleSheet.create({
     parentContainer: {
       backgroundColor: CAColors.BG_WHITE,
       flex:1
-    },
+    },item: {
+        backgroundColor: '#f9c2ff',
+        height:100,
+        margin:8,
+      },
+      title: {
+        fontSize: 32,
+      },
 });
   
 export default HomeScreen
