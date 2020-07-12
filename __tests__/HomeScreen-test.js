000d3c604841af9933287cae945a03083e0de70a
Jeyakumaran Mayooresan <mayu3g@gmail.com>
2020-07-13 01:19:39 +0800

    snapshots added

--- __tests__/HomeScreen-test.js
+++ __tests__/HomeScreen-test.js
@@ -5,6 +5,9 @@
 import 'react-native';
 import React from 'react';
 import HomeScreen from '../src/screens/HomeScreen'
+import CACard from '../src/components/CACard'
+import CAHeader from '../src/components/CAHeader'
+import CALinkButton from '../src/components/CALinkButton'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -12,5 +15,21 @@ import renderer from 'react-test-renderer';
 jest.useFakeTimers();
 
 it('renders correctly', async() => {
-  renderer.create(<HomeScreen />);
+  const tree = renderer.create(<HomeScreen />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly CACard', async() => {
+  const tree = renderer.create(<CACard />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly CAHeader', async() => {
+  const tree = renderer.create(<CAHeader />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly CALinkButton', async() => {
+  const tree = renderer.create(<CALinkButton />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
